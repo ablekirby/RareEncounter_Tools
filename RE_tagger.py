951d6d05f99d4691a4e7344b0fec02353b51f989
ablekirby <ablekirby@protonmail.com>
2021-09-14 19:44:43 -0400

    confirm operations with output

--- RE_tagger.py
+++ RE_tagger.py
@@ -7,18 +7,18 @@ from mutagen.id3 import ID3, TPE1, TIT2, TALB, TYER, TRCK, COMM, TCOP, TIT3, USL
 import sys
 from datetime import datetime
 
-
 class reep:
     def __init__(self):
-        self.artist_name = "ablekirby and coldacid"
+        self.artist_name = "Able Kirby and coldacid"
         self.album_title = "Rare Encounter Podcast"
 
         now = datetime.now()
-        self.date = now.strftime("%Y")
-        self.comment = "Rare Encounter Podcast, with ablekirby and coldacid."
+        self.date = now.strftime("%Y") # All we use for Date is the year
+
+        self.comment = "Rare Encounter Podcast, with Able Kirby and coldacid."
         self.copyright = "Rare Encounter Podcast"
-        self.subtitle = "AbleKirby and coldacid converse on anime they watch, books and manga they read, games they play, and all the tech stuff that they come across."
-        self.unsynced_lyrics = "AbleKirby and coldacid converse on anime they watch, books and manga they read, games they play, and all the tech stuff that they come across."
+        self.subtitle = "Able Kirby and coldacid converse on anime they watch, books and manga they read, games they play, and all the tech stuff that they come across."
+        self.unsynced_lyrics = "Able Kirby and coldacid converse on anime they watch, books and manga they read, games they play, and all the tech stuff that they come across."
         self.track_num = 0
         self.track_title = "Untitled Encounter"
 
@@ -61,5 +61,10 @@ if len(sys.argv) != 4:
     print("\nUsage: RE_tagger.py [mp3 file name] [encounter number] [title]\n\n")
 
 else:
+    print("Tagging " + sys.argv[1] + " With ID3 info.")
+    print("Encounter Number is " + sys.argv[2])
+    print("Encounter Title: " + sys.argv[3])
     myreep = reep.new(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+    print("Writing to disk...")
     myreep.write_tags()
+    print("Done.")
